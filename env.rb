a1fee51b6a39939c42ac6a7f387f9331b5b0c797
Andrii Trebukhin <andrewtrebuhin@gmail.com>
2018-01-26 17:38:35 +0200

    Create env.rb config file.

---
+++ env.rb
@@ -0,0 +1,4 @@
+# Remove this file if you want to run this app on production server
+# And set environment variable with name 'SECRET_KEY' on your prod. server
+
+ENV['SECRET_KEY'] = 'There must be 32 byte size secret key'
